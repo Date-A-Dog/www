72c741ce6e87626cc55b1e880d3b62bb6827cfc7
Ubuntu <ubuntu@ip-10-0-0-224.us-west-2.compute.internal>
2016-11-01 16:54:23 +0000

    production commit

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/www/'));
+app.use(express.static(__dirname + '/www/css'));
+app.use(express.static(__dirname + '/www/js'));
+app.use(express.static(__dirname + '/www/images'));
 
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+var index = ['/', '/index.html', 'index.htm'];
+
+app.get(index, function(req, res) {
+	res.sendFile(__dirname + '/www/index.html');
 });
 
 app.get('/history.html', function(req, res) {
-	res.sendFile(__dirname + '/history.html');
+	res.sendFile(__dirname + '/www/history.html');
 });
 
 app.get('/logout.html', function(req, res) {
-	res.sendFile(__dirname + '/logout.html');
+	res.sendFile(__dirname + '/www/logout.html');
 });
 
 app.listen(3000, function(){
