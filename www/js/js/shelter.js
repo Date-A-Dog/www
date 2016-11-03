42c326127530f366f1808f198898a41b939e4826
Hugo Salazar <hugo.salazarsoto@gmail.com>
2016-11-03 12:01:25 -0700

    Modified shelter methods to interact with updatetable.js

--- www/js/js/shelter.js
+++ www/js/js/shelter.js
@@ -4,15 +4,15 @@
  * and methods to query and update server data.
  *
  */
-var Shelter = function(id) {
+var Shelter = function(_id) {
 
-  //var id = id;
-  var dateRequests = null;
+ 
   var fetchComplete; // avoid calling methods
+
   
   var shelter = {
-    _id: id,
-    _dateRequests: dateRequests
+    id: _id,
+    dateRequests: null
   };
 
   // function which will reset list of requests for this
@@ -25,24 +25,20 @@ var Shelter = function(id) {
     // call async method to load dateRequext data
     fetchRequests('js/js/mockData/dateRequestData.json', function(data){
         //return data here
-        shelter._dateRequests = data;
+        shelter.dateRequests = data;
         fetchComplete = true;
     });  
   };
 
-  // scan through this.dateRequests and extract
-  // dates with status = 'P' - pending 
-  // returns and array of DateRequests sorted 
-  // by date
+  // Update table with date requests with status = 'P'
+  // for this shelter
   shelter.getPendingRequests = function(tableId) {
-    putRequestsOnTable(tableId, 'pending'); // only put "pending"
+    updateTable(tableId, shelter.requests, 'pending'); // only put "pending"
   };
 
-  // Scan through this.dateRequests and extract 
-  // dates with status != 'P' - approved, denied
-  // returns an array of DateRequests sorted by date
+  // Update table with date requests of status = 'A' or 'D'
   shelter.getHistoryRequests = function(tableId) {
-    putRequestsOnTable(tableId, 'history'); // only historic 
+    updateTable(tableId, shelter.requests, 'history'); // only historic 
   };
  
   // Makes appropiate call to database/API to update
@@ -52,55 +48,13 @@ var Shelter = function(id) {
 
   };
 
-  // Returns array of dateRequests
+  // Returns array of all dateRequests for this shelter
   shelter.getDateRequests = function () {
-    return shelter._dateRequests;
+    return shelter.dateRequests;
   };
 
 
-  function putRequestsOnTable(tableId, status) {
 
-    var table = document.getElementById(tableId);
-
-    while(table.rows.length > 1) {
-      table.deleteRow(1);
-    }
-
-    // iterate through each request in array
-    for (var i = 0; i < shelter._dateRequests.length; i++) {
-      var request = shelter._dateRequests[i];
-
-      if (status === 'pending') {
-        // skip if request is not 'pending' status
-        if (request['status'] != 'P') continue;
-      } else if (status === 'history') {
-        // otherwise only skip 'pending'
-        if (request['status'] === 'P') continue;
-      }
-      // new row
-      var row = document.createElement('tr');
-      //  requests properties
-      var properties = ['id', 'daterProfile', 'dogProfile', 'dateTime', 'status'];
-
-      // append each prop to current row
-      for (var j = 0; j < properties.length; j++) {
-        
-        var cell = document.createElement('td');
-        // set properties
-        if (j === 1) {  // extract user name
-          cell.innerHTML = request[properties[j]].fName + " " + request[properties[j]].lName;
-        } else if (j === 2) { //extract dog name
-          cell.innerHTML = request[properties[j]].name;
-        } else {
-          cell.innerHTML = request[properties[j]];
-        }
-        // append new cell
-        row.append(cell);
-      }
-      // append the new row
-      table.append(row);
-    }
-  };
 
   return shelter;
 };
