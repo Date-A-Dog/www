c3b821fd85000e1abf945f92d13144dece2f4f23
Alexis <laetaku@uw.edu>
2016-11-04 02:48:33 -0700

    html files, css, and js in progress for requests and shelter dogs pages

---
+++ www/js/js/accordion.js
@@ -0,0 +1,9 @@
+$('.accordion-container').each(function(){
+  var thead = $(this).find('thead');
+  var tbody = $(this).find('tbody');
+  
+  tbody.hide();
+  thead.click(function(){
+    tbody. slideToggle();
+  })
+})
