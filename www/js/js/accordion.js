35a6c20ec6146488721174d6edd2468668dffb51
Alexis <laetaku@uw.edu>
2016-11-07 06:09:14 -0800

    bootstrap nav bar with our css being applied with priority, accordian works

--- www/js/js/accordion.js
+++ www/js/js/accordion.js
@@ -1,10 +1,14 @@
-$('.accordion-container').each(function(){
-  var thead = $(this).find('thead');
-  var tbody = $(this).find('tbody');
-  
-  tbody.hide();
-  thead.click(function(){
-    tbody.slideToggle();
-  })
-})
+$(function(){
+	$('.accordion-container').each(function(){
+	  var thead = $(this).find('thead');
+	  var tbody = $(this).find('tbody');
+	  
+	  tbody.hide();
+	  thead.click(function(){
+		$(this).parent().siblings().find('tbody').slideUp();
+		tbody.slideToggle();
+		
+	  })
+	})
+});
 
